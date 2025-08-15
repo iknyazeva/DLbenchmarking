a0909a1e2cf9235191a309dd65335059833e6bb0
myxa <tanai2010@mail.ru>
2025-08-15 19:01:11 +0300

    add EOEC dataset

--- source/models/meanmlp.py
+++ source/models/meanmlp.py
@@ -39,6 +39,7 @@ class MeanMLP(nn.Module):
         num_layers = cfg.model.num_layers
         output_size = cfg.model.output_size
 
+        #if len(hidden_size) != 0:
 
         # input block
         layers = [
@@ -70,11 +71,13 @@ class MeanMLP(nn.Module):
         self.fc = nn.Sequential(*layers)
 
     def forward(self, x: torch.Tensor, introspection=False):
+        #print("!!!!!!!!!!!!!!!!!!!!1x.shape", x.shape)
+        x = x.permute(0, 2, 1)
         bs, tl, fs = x.shape  # [batch_size, time_length, input_feature_size]
 
         # view: Теперь каждый временной шаг в каждом объекте батча 
         # становится отдельным "экземпляром" для пропуска через слои сети.
-        fc_output = self.fc(x.view(-1, fs))
+        fc_output = self.fc(x.reshape(-1, fs))
         fc_output = fc_output.view(bs, tl, -1)
 
         logits = fc_output.mean(1)
